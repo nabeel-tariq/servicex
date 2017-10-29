52cf304332230eab2ba23c09bba3d449d43dbc77
Muhammad Nabeel <nabeeltariq355@gmail.com>
2017-10-29 15:58:11 +0500

    paperclip issue fix

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
 class Attachment < ApplicationRecord
   belongs_to :attachable, polymorphic: true
   validates_presence_of :name, :image_type
-  has_attached_file :document,default_url: "#{APP_URL}/missing.png",preserve_files: true, :styles => lambda { |a| a.instance.is_image? ? {medium: '300x300>', thumb: '100x100>'} : {} },
+  has_attached_file :document,default_url: "#{APP_URL}/missing.png", :styles => {:thumb => "100x100#", :small  => "150x150>", :medium => "200x200" },
                     :url => "#{APP_URL}/system/:class/:attachment/:image_type/:hash/:filename",
                     :path => lambda { |a| ":rails_root/public/system/:class/:attachment/:image_type/:hash/:filename" },
                     hash_secret: "6711203d2886152b7ed71ec9cfc79e1d51aab530b2e3e4eb46734e1180bdade407289c46bc17426e69a1cd95f07cbe9e7a3e7261017b59ae88b0b9b0b380e84c"
