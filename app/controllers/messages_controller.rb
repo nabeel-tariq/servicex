f84c2ba3e92a0f0de15d35f5b4b127790ea52d5e
Muhammad Nabeel <nabeeltariq355@gmail.com>
2017-12-11 07:24:54 +0500

    error handled fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,13 @@ class MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], (params[:message][:subject] || current_user.full_name)).conversation
-    flash[:success] = "Message has been sent!"
-    redirect_to conversation_path(conversation)
+    if conversation
+      flash[:success] = "Message has been sent!"
+      redirect_to conversation_path(conversation)
+    else
+      flash[:error] = "Message has not been sent!"
+      redirect_to :back
+    end
+
   end
 end
